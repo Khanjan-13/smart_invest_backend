262e3d8f53a8be1e4573e3e64e97b746052832e4
Khanjan <khanjansoni.ks07@gmail.com>
2026-03-26 00:09:39 +0530

    bug fixed

--- src/controllers/share_market/indian_api.js
+++ src/controllers/share_market/indian_api.js
@@ -147,21 +147,21 @@ exports.getHistoricalStockData = async (req, res) => {
 
 exports.getMutualFundDetails = async (req, res) => {
   try {
-    const { fund_name } = req.query;
+    const { stock_name } = req.query;
 
     // Random API key
     const randomKey = apiKeys[Math.floor(Math.random() * apiKeys.length)];
 
     // Validation
-    if (!fund_name) {
+    if (!stock_name) {
       return res.status(400).json({
         success: false,
-        message: "fund_name is required"
+        message: "stock_name is required"
       });
     }
 
     // API URL
-    const url = `https://stock.indianapi.in/mutual_funds_details?fund_name=${encodeURIComponent(fund_name)}`;
+    const url = `https://stock.indianapi.in/mutual_funds_details?stock_name=${encodeURIComponent(stock_name)}`;
 
     const response = await axios.get(url, {
       headers: {
