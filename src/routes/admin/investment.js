77ae63ee2494832a262329bc5a2a9f9edb03d838
Khanjan <khanjansoni.ks07@gmail.com>
2026-04-04 01:04:48 +0530

    bug fixed

--- src/routes/admin/investment.js
+++ src/routes/admin/investment.js
@@ -1,3 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllInvestments,
+  getGlobalStats,
+} = require("../../controllers/admin/investment");
+
 router.get("/", getAllInvestments);
 router.get("/stats", getGlobalStats);
 
